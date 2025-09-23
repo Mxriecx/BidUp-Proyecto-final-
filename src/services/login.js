36463acf0cd0d1fa526c72f88b137fdd9fbdc283
Fabo <fabianquiro05@hotmail.com>
2025-09-22 21:19:23 -0500

    [mi intento de codigo]merge

---
+++ src/services/login.js
@@ -0,0 +1,65 @@
+import { userModel } from "../models/users.model.js"; //validar usuario
+import { generateToken } from "../config/jwt.js";//generar el token de seguridad
+import bcryptjs from "bcryptjs"; //validar la contraseña
+
+export const login = async (request, response) =>{
+    try {
+        // VALIDACIÓN 1: Si el correo existe en la base de datos
+        const {emailLogin, passwordLogin} = request.body;
+
+        // 1. es buscar en la base de datos
+        const userFound = await userModel.findOne({
+            email: emailLogin
+        });
+
+        console.log("usuario encontrado: ", userFound);
+
+        if(!userFound){
+            return response.status(404).json({
+                "mensaje": "Usuario no encontrado, por favor regístrate"
+            });
+        }
+
+
+        // VALIDACIÓN 2 : contraseña correcta
+        const validPassword = await bcryptjs.compare(passwordLogin, userFound.password); //true o false
+        if(!validPassword){
+            return response.status(401).json({
+                "mensaje": "Contraseña incorrecta"
+            });
+        }
+
+        // GENERACIÓN DE TOKEN -> Verificar permisos
+        const payload = {
+            id: userFound._id,
+            user: userFound.username
+        }
+
+        if(userFound.role === "admin"){
+            payload.admin = true;
+        }else{
+            payload.admin = false;
+        }
+
+        const token = await generateToken(payload);
+        console.log("payload: ", payload);
+        console.log("token", token);
+
+
+        return response.status(200).json({
+            "mensaje": "Inicio de sesión exitoso",
+            "token": token
+        });
+
+
+    } catch (error) {
+         return response.status(400).json({
+            "mensaje": "Ocurrió un error al iniciar sesión",
+            "error": error.message || error //alt + 124 o  alt gr + 1
+        })
+    }
+}
+
+
+
+

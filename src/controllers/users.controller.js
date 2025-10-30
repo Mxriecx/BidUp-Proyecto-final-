07da4f7951515a4d86c044798cae9700c1474603
Maria Paz <paztuiranmaria@gmail.com>
2025-10-29 22:26:15 -0500

    multer changes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,105 +1,93 @@
 import { userModel } from "../models/users.model.js";
 import bcryptjs from "bcryptjs";
 
-export const postUser = async (request,response) => {
-
-     try{
-         // deestructuracion se hace para procesar la informacion del usuario antes de guardarla 
-
-        const {name,age,username,password,role,email,nickname,tenure,image}=request.body;
-
-        const codedPassword = await bcryptjs.hash(password,8);
-         
-        await userModel.create({
-            name,
-            age,
-            username,
-            password:codedPassword,
-            email,
-            role,
-            nickname,
-            tenure,
-            image
-
-        });
-
-        return response.status(201).json({
-            "mensaje":"usuario creado correctamente"
-        })
-
-    }catch (error){
-        return response.status(500).json({
-            "mensaje":"Error al crear el usuario",
-            "error": error.message || error            
-
-        })
-
+export const postUser = async (request, response) => {
+  try {
+    // deestructuracion se hace para procesar la informacion del usuario antes de guardarla
+    if (!request.file) {
+      return response.status(400).json({
+        mensaje: "Debes subir un archivo de imagen",
+      });
     }
+    const codedPassword = await bcryptjs.hash(request.body.password, 8);
 
-}
+    const newUser = {
+      ...request.body,
+      image: `/uploads/${request.file.filename}`,
+      password : codedPassword
+    };
+     
+    await userModel.create(newUser);
 
-//2. metodo para mostrar todos los productos -> GET 
+    return response.status(201).json({
+      mensaje: "usuario creado correctamente",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "Error al crear el usuario",
+      error: error.message || error,
+    });
+  }
+};
 
-export const getUser = async (request,response) => {
-    try{
-        const allUsers =await userModel.find();
+//2. metodo para mostrar todos los productos -> GET
 
-        return response.status(200).json({
-            "mensaje":"se encontraron todos los usuarios",
-            "data": allUsers
-        })
-    }catch(error){
-        return response.status(500).json({
-        "mensaje":"ocurrio un error al mostrar usuarios",
-        "error":error.message || error
+export const getUser = async (request, response) => {
+  try {
+    const allUsers = await userModel.find();
 
-    })
-}
-}
-
+    return response.status(200).json({
+      mensaje: "se encontraron todos los usuarios",
+      data: allUsers,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "ocurrio un error al mostrar usuarios",
+      error: error.message || error,
+    });
+  }
+};
 
 //3. metodo para actualizar  un producto -> PUT
 
-export const putUserById = async (request,response) => {
-    try{
+export const putUserById = async (request, response) => {
+  try {
+    const idForUserUpdate = request.params.id;
+    const dataForUserUpdate = request.body;
 
-        const idForUserUpdate = request.params.id;
-        const dataForUserUpdate = request.body;
+    if (dataForUserUpdate.password) {
+      dataForUserUpdate.password = await bcryptjs.hash(
+        dataForUserUpdate.password,
+        8
+      );
+    }
 
-         if (dataForUserUpdate.password) {
-            dataForUserUpdate.password = await bcryptjs.hash(dataForUserUpdate.password,8);
-        }
-
-        await userModel.findByIdAndUpdate (idForUserUpdate,dataForUserUpdate);
-        return response.status(200).json({
-            "mensaje" :"Usuario actualizado correctamente"
-        });
-
-    }catch(error){
-         return response.status(500).json({
-        "mensaje":"ocurrio un error al actualizar el usuario",
-        "error":error.message || error
-    })
-}
-}
-
+    await userModel.findByIdAndUpdate(idForUserUpdate, dataForUserUpdate);
+    return response.status(200).json({
+      mensaje: "Usuario actualizado correctamente",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "ocurrio un error al actualizar el usuario",
+      error: error.message || error,
+    });
+  }
+};
 
 //4. metodo para Eliminar un producto -> DELETE
 
-export const deleteUserById =async (request,response)=>{
-    try{
+export const deleteUserById = async (request, response) => {
+  try {
+    const idForDelete = request.params._id;
 
-        const idForDelete = request.params._id;      
-
-        await userModel.findByIdAndDelete (idForDelete);
-        return response.status(200).json({
-            "mensaje" :"Usuario eliminado correctamente"
-        });
-
-    }catch(error){
-         return response.status(500).json({
-        "mensaje":"ocurrio un error al  eliminar el usuario",
-        "error":error.message || error
-    })
-}
-} 
+    await userModel.findByIdAndDelete(idForDelete);
+    return response.status(200).json({
+      mensaje: "Usuario eliminado correctamente",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      mensaje: "ocurrio un error al  eliminar el usuario",
+      error: error.message || error,
+    });
+  }
+};
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,8 +2,48 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 
+    
+    image: {
+        type: String,
 
-   
+    },
+    name: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+
+    },
+
+    email: {
+        type: String,
+        required: true
+
+    },
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    nickname: {
+        type: String
+
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["admin","user"],
+        required: true
+    },
 
     tenure: {
         type: Number
@@ -17,6 +57,7 @@ const userSchema = new mongoose.Schema({
 
     }
 
+   
 
 
 })
